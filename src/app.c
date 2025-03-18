4a82f1b93b58b008fa70f805251dafb4149ba007
Sam Christensen <samochristensen@gmail.com>
2025-03-18 17:29:45 -0600

    add func to verify qspi config - modify TC by state - add gitignore

--- src/app.c
+++ src/app.c
@@ -1,10 +1,43 @@
 #include "app.h"
 #include "definitions.h"
 
+/* Callbacks */
 void toggle_user_led(){
     GPIO_PC18_Toggle();
 }
 
+/* Support Functions */
+bool verify_qspi_clocks(){
+    // QSPI Clock Verification
+    uint32_t apb_mask = MCLK_REGS->MCLK_APBAMASK;
+    uint32_t ahb_mask = MCLK_REGS->MCLK_AHBMASK;
+    uint32_t gclk_qspi = GCLK_REGS->GCLK_PCHCTRL[39];
+
+    // Assume clock verification must pass for initialization
+    if ((apb_mask & MCLK_APBCMASK_QSPI_Msk) &&
+        (ahb_mask & MCLK_AHBMASK_QSPI_Msk) &&
+        (ahb_mask & MCLK_AHBMASK_QSPI_2X_Msk) &&
+        (gclk_qspi & GCLK_PCHCTRL_CHEN_Msk))
+    {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool verify_qspi_baud() {
+    uint32_t qspi_baud = QSPI_REGS->QSPI_BAUD;
+
+    // Extract clock polarity (CPOL bit)
+    bool clock_polarity = (qspi_baud & QSPI_BAUD_CPOL_Msk) ? true : false;
+    bool expected_polarity = false;
+
+    if (clock_polarity != expected_polarity) {
+        return false;
+    }
+
+    return true;
+}
 
 APP_DATA appData;
 
@@ -22,24 +55,22 @@ void APP_Tasks(void)
     switch (appData.state)
     {
         case APP_STATE_INIT:
-        {
-            bool is_initialized = true;
+        {    
+            if (!verify_qspi_clocks()) break;
+            if (!verify_qspi_baud()) break;
             
-            if (is_initialized)
-            {
-                appData.state = APP_STATE_RUNNING;
-            }
-            break;
+            appData.state = APP_STATE_RUNNING;
         }
         case APP_STATE_RUNNING:
         {
             // Application's main loop logic
-            
+            TC0_Timer16bitCounterSet(15000); // 500ms = 29295
             break;
         }
         case APP_STATE_ERROR:
         {
             // Handle errors (could add a debug message)
+            TC0_Timer16bitCounterSet(60000); // 500ms = 29295
             break;
         }
         default:
@@ -47,4 +78,6 @@ void APP_Tasks(void)
             break;
         }
     }
+    
+    
 }
