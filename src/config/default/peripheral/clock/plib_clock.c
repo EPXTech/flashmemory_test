275e6976b29c2adebb2ada837202ed4eef9eb820
Sam Christensen <samochristensen@gmail.com>
2025-03-18 17:29:59 -0600

    config clock for qspi peripheral

--- src/config/default/peripheral/clock/plib_clock.c
+++ src/config/default/peripheral/clock/plib_clock.c
@@ -164,6 +164,19 @@ void CLOCK_Initialize (void)
 
     /* Configure the APBA Bridge Clocks */
     MCLK_REGS->MCLK_APBAMASK = 0x47ffU;
+    
+    /* Enable QSPI GCLK  */
+    GCLK_REGS->GCLK_GENCTRL[3] = GCLK_GENCTRL_SRC_DFLL |
+                                 GCLK_GENCTRL_GENEN_Msk |
+                                 GCLK_GENCTRL_DIV(3);
+    
+    GCLK_REGS->GCLK_PCHCTRL[39] = GCLK_PCHCTRL_GEN(3) | GCLK_PCHCTRL_CHEN_Msk;
 
+    while ((GCLK_REGS->GCLK_PCHCTRL[39] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
+    {
+        /* Wait for synchronization */
+    }
+
+    MCLK_REGS->MCLK_AHBMASK |= MCLK_AHBMASK_QSPI_2X_Msk;
 
 }
